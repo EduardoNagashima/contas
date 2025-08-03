321035fcff241c1e98279c3a0486fc4c8a1450a0
Eduardo Yudji Nagashima Miyata <45578435+EduardoNagashima@users.noreply.github.com>
2025-08-03 20:53:33 +0000

    remove obsolete information

--- index.js
+++ index.js
@@ -1,45 +1,14 @@
 import inquirer from 'inquirer';
 
-async function getValues(isAnna = true) {
-
+async function getValues() {
   const perguntas = [
-    {
-      type: 'input',
-      name: 'salarioPrimeiraPessoa',
-      message: 'Salário da primeira pessoa (Eduardo):',
-      validate: (input) => {
-        const valor = input.replace(',', '.');
-        if (valor === '') return true;
-        if ((isNaN(Number(valor)) || Number(valor) < 0)) {
-          return 'Por favor, insira um valor válido.';
-        }
-        return true;
-      },
-      filter: (input) =>
-        input === '' ? 0 : parseFloat(Number(input.replace(',', '.'))),
-    },
-    {
-      type: 'input',
-      name: 'salarioSegundaPessoa',
-      message: 'Salário da segunda pessoa (Anna):',
-      validate: (input) => {
-        const valor = input.replace(',', '.');
-        if (valor === '') return true;
-        if ((isNaN(Number(valor)) || Number(valor) < 0)) {
-          return 'Por favor, insira um valor válido.';
-        }
-        return true;
-      },
-      filter: (input) =>
-        input === '' ? 0 : parseFloat(Number(input.replace(',', '.'))),
-    },
     {
       type: 'input',
       name: 'mercado',
       message: 'Valor do mercado:',
-       validate: (input) => {
+      validate: (input) => {
         const valor = input.replace(',', '.');
-        if (valor === '' || (isNaN(Number(valor)) || Number(valor) < 0)) {
+        if (valor === '' || isNaN(Number(valor)) || Number(valor) < 0) {
           return 'Por favor, insira um valor válido.';
         }
         return true;
@@ -50,9 +19,9 @@ async function getValues(isAnna = true) {
       type: 'input',
       name: 'aluguel',
       message: 'Valor do aluguel:',
-       validate: (input) => {
+      validate: (input) => {
         const valor = input.replace(',', '.');
-        if (valor === '' || (isNaN(Number(valor)) || Number(valor) < 0)) {
+        if (valor === '' || isNaN(Number(valor)) || Number(valor) < 0) {
           return 'Por favor, insira um valor válido.';
         }
         return true;
@@ -63,9 +32,9 @@ async function getValues(isAnna = true) {
       type: 'input',
       name: 'luz',
       message: 'Valor da luz:',
-       validate: (input) => {
+      validate: (input) => {
         const valor = input.replace(',', '.');
-        if (valor === '' || (isNaN(Number(valor)) || Number(valor) < 0)) {
+        if (valor === '' || isNaN(Number(valor)) || Number(valor) < 0) {
           return 'Por favor, insira um valor válido.';
         }
         return true;
@@ -76,9 +45,9 @@ async function getValues(isAnna = true) {
       type: 'input',
       name: 'condominio',
       message: 'Valor do condomínio:',
-       validate: (input) => {
+      validate: (input) => {
         const valor = input.replace(',', '.');
-        if (valor === '' || (isNaN(Number(valor)) || Number(valor) < 0)) {
+        if (valor === '' || isNaN(Number(valor)) || Number(valor) < 0) {
           return 'Por favor, insira um valor válido.';
         }
         return true;
@@ -97,25 +66,15 @@ async function getValues(isAnna = true) {
   try {
     const respostas = await inquirer.prompt(perguntas);
 
-    calcularValores(respostas, isAnna);
-  } catch {
+    calcularValores(respostas);
+  } catch (err) {
+    console.error('Erro:', err);
     return;
   }
 }
 
-function calcularValores(respostas, isAnna) {
-  const {
-    salarioPrimeiraPessoa,
-    salarioSegundaPessoa,
-    mercado,
-    aluguel,
-    luz,
-    condominio,
-    internet,
-  } = respostas;
-
-  const CASTELINHO = 34.685; // Valor fixo
-  const GUARDA_ROUPA = 102.47; // Valor fixo
+function calcularValores(respostas) {
+  const { mercado, aluguel, luz, condominio, internet } = respostas;
 
   const METADE_MERCADO = parseFloat(mercado) / 2;
 
@@ -124,33 +83,19 @@ function calcularValores(respostas, isAnna) {
     parseFloat(luz) +
     parseFloat(condominio) +
     parseFloat(internet);
-  const TOTAL_RENDA =
-    parseFloat(salarioPrimeiraPessoa) + parseFloat(salarioSegundaPessoa);
 
   let RENDA_PERCENTUAL_PRIMEIRA_PESSOA = 0.54;
   let RENDA_PERCENTUAL_SEGUNDA_PESSOA = 0.46;
 
-  if (salarioPrimeiraPessoa && salarioSegundaPessoa) {
-    RENDA_PERCENTUAL_PRIMEIRA_PESSOA =
-      parseFloat(salarioPrimeiraPessoa) / TOTAL_RENDA;
-    RENDA_PERCENTUAL_SEGUNDA_PESSOA =
-      parseFloat(salarioSegundaPessoa) / TOTAL_RENDA;
-  }
-
   const VALOR_PRIMEIRA_PESSOA = RENDA_PERCENTUAL_PRIMEIRA_PESSOA * TOTAL_CONTAS;
-
   const VALOR_SEGUNDA_PESSOA = RENDA_PERCENTUAL_SEGUNDA_PESSOA * TOTAL_CONTAS;
-
-  const VALOR_TOTAL_SEGUNDA_PESSOA =
-    VALOR_SEGUNDA_PESSOA + METADE_MERCADO + CASTELINHO + GUARDA_ROUPA;
+  const VALOR_TOTAL_SEGUNDA_PESSOA = VALOR_SEGUNDA_PESSOA + METADE_MERCADO;
 
   console.log(
     `TOTAL: R$${parseFloat(VALOR_PRIMEIRA_PESSOA + VALOR_SEGUNDA_PESSOA).toFixed(2)}`
   );
   console.log(`Valor de Eduardo: R$${VALOR_PRIMEIRA_PESSOA.toFixed(2)}`);
-  console.log(
-    `Valor de Anna: R$${VALOR_TOTAL_SEGUNDA_PESSOA.toFixed(2)} \nMês: R$${parseFloat(VALOR_SEGUNDA_PESSOA).toFixed(2)}, Mercado: R$${METADE_MERCADO}, Castelinho e Guarda Roupa: R$${CASTELINHO} + R$${GUARDA_ROUPA}`
-  );
+  console.log(`Valor de Anna: R$${VALOR_TOTAL_SEGUNDA_PESSOA.toFixed(2)}`);
 }
 
 getValues();
